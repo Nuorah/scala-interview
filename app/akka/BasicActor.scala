32420344d5df0f35f953695464c85e452d154306
Haroun Moatassime <haroun.moatassime@gmail.com>
2023-09-28 18:28:20 +0100

    akka

--- app/akka/BasicActor.scala
+++ app/akka/BasicActor.scala
@@ -8,17 +8,34 @@ import akka.actor.{ Actor, ActorSystem, Props }
  * When receiving a message that says "Hello", BasicActor must print "Hello there."
  * It must print "What?" when receiving any other message
  */
-class BasicActor {}
 
+object BasicActor {
+  final case class Hello(message:String);
+
+}
+class BasicActor extends Actor {
+  import BasicActor._
+
+  override def receive: Receive = {
+    case Hello("Hello") => println("Hello there.")
+    case Hello(_) => println("What?")
+  }
+
+}
+
+//sbt "runMain com.particeep.test.akka.FireActor"
 object FireActor {
 
+  import BasicActor._
   /**
    * Create an instance of BasicActor
    *
    * Make it print "Hello there." and "What?"
    */
-  def fireActor(): Unit = {
-    //    val system = ActorSystem("Actor System")
-    //    val basic_actor = system.actorOf(Props[BasicActor], name = "basic actor")
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("actor_system")
+    val basic_actor = system.actorOf(Props[BasicActor], name = "basic_actor")
+    basic_actor ! Hello("Hello")
+    basic_actor ! Hello("Sunny day innit?") 
   }
 }
--- app/akka/WhatsWrong3.scala
+++ app/akka/WhatsWrong3.scala
@@ -11,6 +11,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * What would you do to fix it ?
  * Do not mind about the not implemented code
  */
+
+//onComplete might call on a different thread than the actor, akka cannot then garantee in what
+//order internalState will be mutated. To fix it when the future is completed send a message to 
+//self to then mutate internal state
+
 class WhatsWrong3 extends Actor {
 
   var internalState = "internal state"
